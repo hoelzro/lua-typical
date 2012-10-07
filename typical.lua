f3d5584d76af8861224233944a2c73f730ee7582
Rob Hoelz <rob@hoelz.ro>
2012-10-08 00:14:07 +0200

    Add typical.iscallable

--- typical.lua
+++ typical.lua
@@ -38,6 +38,16 @@ function typical.type(value)
   return rawtype(value)
 end
 
+function typical.iscallable(value)
+  if rawtype(value) == 'function' then
+    return true
+  end
+
+  local value_mt = getmetatable(value)
+
+  return value_mt and value_mt.__call
+end
+
 function mt:__call(...)
   return self.type(...)
 end
