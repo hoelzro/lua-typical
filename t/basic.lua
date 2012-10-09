9047a79d8d5b4dfaf3aa29a106bd6f878ac7424b
Rob Hoelz <rob@hoelz.ro>
2012-10-09 21:34:17 +0200

    Add test file

---
+++ t/basic.lua
@@ -0,0 +1,34 @@
+local typical = require 'typical'
+
+pcall(require, 'Test.More')
+require 'Test.More'
+
+plan(10)
+
+do -- basic values (should behave like type() )
+  is(typical(nil), 'nil')
+  is(typical(true), 'boolean')
+  is(typical(1), 'number')
+  is(typical('foo'), 'string')
+  is(typical(function() end), 'function')
+  is(typical({}), 'table')
+  is(typical(coroutine.create(function() end)), 'thread')
+
+  if type(_G.newproxy) == 'function' then
+    is(typical(newproxy(true)), 'userdata')
+  else
+    skip 'newproxy is needed for testing userdata'
+  end
+end
+
+do -- __type metafield
+  local value = setmetatable({}, { __type = 'object' })
+
+  is(typical(value), 'object')
+
+  value = setmetatable({ _class = 'something' }, { __type = function(self)
+    return self._class
+  end})
+
+  is(typical(value), 'something')
+end
